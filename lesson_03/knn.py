b7c5c32a3172b575d2f64d2159f9d7ac2610d7c4
DaSheng <excelchart@sina.cn>
2018-04-03 18:28:51 +0800

    first ml KNN

---
+++ lesson_03/knn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# encoding: utf-8
+from __future__ import print_function
+
+"""
+author: 大圣
+contact: 626494970@qq.com
+@file: knn.py
+@time: 2018/4/3 0003 下午 6:26
+
+"""
+class KNN(object):
+    def __init__(self):
+        pass
